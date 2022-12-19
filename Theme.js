f6d388e16d33c5318cd4bb1fa5182f4ae0521ec1
KevinsienHeldrayetno <103363748+KevinsienHeldrayetno@users.noreply.github.com>
2022-12-19 13:26:19 +0700

    Add files via upload

---
+++ Theme.js
@@ -0,0 +1,18 @@
+const theme = {
+  light: {
+    theme: "light",
+    color: "black",
+    background: "#FFFFFF",
+    placeholderTextColor: "black",
+    borderBottomColor: "black",
+  },
+  dark: {
+    theme: "dark",
+    color: "white",
+    background: "#1E1E1E",
+    placeholderTextColor: "white",
+    borderBottomColor: "white",
+  },
+};
+
+export default theme;
